ae7558787e183b80c92c09c58df3bc57ace745b0
JasonCodeWaves <jason@codewaves.co.za>
2024-10-01 11:41:03 +0200

    updates

--- app_setup.py
+++ app_setup.py
@@ -6,14 +6,7 @@ from aiohttp_middlewares import error_middleware, timeout_middleware
 
 def create_app():
     # Define allowed origins for CORS
-    cors_config = {
-        "*": {  # This allows all origins
-            "allow_credentials": True,
-            "allow_methods": ["GET", "POST", "OPTIONS"],  # Specify allowed HTTP methods
-            "allow_headers": ["api-key", "date", "Content-Type", "Authorization"],  # Allowed headers
-            "expose_headers": ["Content-Type"],  # Headers that can be exposed to the browser
-        }
-    }
+    cors_config = {}
 
     # Create the application
     app = web.Application()
@@ -23,8 +16,7 @@ def create_app():
 
     # Add CORS middleware allowing all origins
     app.middlewares.append(cors_middleware(
-        allow_all=True,  # Allow all origins
-        origins=cors_config  # Still specifying the allowed methods and headers
+      
     ))
 
     # Add timeout middleware (optional)
--- middleware.py
+++ middleware.py
@@ -6,42 +6,16 @@ async def main_middleware(app, handler):
     
     async def middleware_handler(request):
 
-            print(f"🟢URL: {request.url}")
-            print(f"🟢Method: {request.method}")
-            print(f"🟢Headers: {request.headers}")
-            print(f"🟢Query: {request.query}")
-            print(f"🟢Match info: {request.match_info}")
-            print(f"🟢Body exists: {request.body_exists}")
-            print(f"🟢Content type: {request.content_type}")
-            print(f"🟢Content length: {request.content_length}")
-            
-            # Handle CORS preflight requests (OPTIONS)
-            if request.method == 'OPTIONS':
-                headers = {
-                    "Access-Control-Allow-Origin": "*",  # Or specific domain 'http://localhost:5173'
-                    "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-                    "Access-Control-Allow-Headers": "api-key, Content-Type, Authorization, date_req",
-                }
-                return web.Response(status=200, headers=headers)
-            
-
             # check headers 'api-key' == '123'
-            if request.headers.get('api-key') == '123':
-                
- 
-                
+            if request.headers.get('api_key') == '123':
                 try:
                  # Call the CORS middleware handler
                     response = await handler(request)
                     return response
-            
                 except asyncio.TimeoutError:
                     return web.Response(text="Request timed out", status=504)
-                
             else:
                 return web.Response(text="API key is missing or invalid", status=401)
                
- 
-          
     
     return middleware_handler
