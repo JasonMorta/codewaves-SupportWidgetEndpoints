bca1fce1b5214b36f8ad9c1663df0c85ddbbf04f
JasonCodeWaves <jason@codewaves.co.za>
2024-10-01 12:21:21 +0200

    updates

--- middleware.py
+++ middleware.py
@@ -2,19 +2,21 @@ import asyncio
 
 
 async def main_middleware(app, handler):
-    
+
     async def middleware_handler(request):
 
-            # check headers 'api-key' == '123'
-            if request.headers.get('api_key') == '123':
-                try:
-                 # Call the CORS middleware handler
-                    response = await handler(request)
-                    return response
-                except asyncio.TimeoutError:
-                    return web.Response(text="Request timed out", status=504)
-            else:
-                return web.Response(text="API key is missing or invalid", status=401)
-               
-    
+        # # check headers 'api-key' == '123'
+        # if request.headers.get('api_key') == '123':
+        #     try:
+        #      # Call the CORS middleware handler
+        #         response = await handler(request)
+        #         return response
+        #     except asyncio.TimeoutError:
+        #         return web.Response(text="Request timed out", status=504)
+        # else:
+        #     return web.Response(text="API key is missing or invalid", status=401)
+
+        response = await handler(request)
+        return response
+
     return middleware_handler
