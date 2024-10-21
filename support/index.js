e66208771ccf6016261c21500d83b6d6facf9b1e
JasonCodeWaves <jason.morta23@gmail.com>
2024-10-21 09:24:11 +0200

    updates

--- support/index.js
+++ support/index.js
@@ -24,22 +24,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// get conversation data
-app.get('/conversations', async (req, res) => {
-
-  //req.query.endDate__2024-10-14T23:59 format
-  //req.query.startDate__2024-10-14T23:59 format
-  //req.query.agentId__842946f3-06fa-4edd-ab0d-7f88d554efd6
-
-  try {
-   // const data = await fetchAgentConversationData(req.query.agentId, req.query.startDate, req.query.endDate);
-   const data = await fetchAgentConversationData("842946f3-06fa-4edd-ab0d-7f88d554efd6", "2024-10-01T01:00", "2024-10-21T23:59");
-
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 
 /* ===========================Port Listener============================= */
